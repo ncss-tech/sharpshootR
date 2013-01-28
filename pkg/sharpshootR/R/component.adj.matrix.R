ef34548ae14fb0f6e389f306e3b6b9610d849b14
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-28 18:52:03 +0000

    first edition of the sharpshootR package

---
+++ pkg/sharpshootR/R/component.adj.matrix.R
@@ -0,0 +1,52 @@
+component.adj.matrix <-function(d, mu='mukey', co='compname', wt=NULL) {
+	# extract a list of component names that occur together
+	l <- dlply(d, mu, function(i) unique(i[[co]]))
+	
+	# include only those components that occur with other components
+	mu.multiple.components <- names(which(sapply(l, function(i) length(i) > 1)))
+	u <- unique(d[[co]][d[[mu]] %in% mu.multiple.components])
+	u <- sort(u)
+	
+	# empty adjacency matrix
+	m <- matrix(rep(0, times=length(u)^2), nrow=length(u))
+	dimnames(m) <- list(u, u)
+	
+	# weighted version
+	if(!missing(wt)) {
+		# extract a list of pair-wise weights
+		l.wts <- dlply(d, mu, .pair.wise.wts, wt=wt)
+		
+		for(i in 1:length(l)) {
+			series.vect <- l[[i]] # get current list element
+			wts.mat <- l.wts[[i]]
+			
+			# check to make sure there are more than 1 components in this map unit
+			if(length(series.vect) > 1) {
+				## note that it is the _upper triangle_ that we care about
+				m[series.vect, series.vect] <- m[series.vect, series.vect] + wts.mat
+			}
+		}
+	}
+	
+	# un-weighted version
+	else {
+		for(i in 1:length(l)) {
+			# get current list element
+			series.vect <- na.omit(l[[i]])	
+			# check to make sure there are more than 1 components in this map unit
+			if(length(series.vect) > 1) {
+				## increment affected elements by 1
+				m[series.vect, series.vect] <- m[series.vect, series.vect] + 1			
+			}
+		}
+	}
+	
+	# reset diagonal of the matrix
+	diag(m) <- 0
+	
+	# scale
+	m <- m / max(m, na.rm=TRUE)
+	
+	# done
+	return(m)
+}

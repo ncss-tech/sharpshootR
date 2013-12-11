9dbb58dadb01277d766bdc5a6e263322e683b24d
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-11 23:33:19 +0000

    bug fixes, and updates to generalized function for evaluating a collection of pedons by taxonname

--- pkg/sharpshootR/R/dissimilarity.sketch.R
+++ pkg/sharpshootR/R/dissimilarity.sketch.R
@@ -1,6 +1,6 @@
 # TODO: better generalization:
 #         - generate box-whisker plots on re-scaled axis with custom scales
-dissimilarity.sketch <- function(f.i, n.groups=2, v=c('clay', 'total_frags_pct')) {
+dissimilarity.sketch <- function(x, n.groups=2, v=c('clay', 'total_frags_pct')) {
   require(cluster)
   require(MASS)
   require(ape)
@@ -11,19 +11,20 @@ dissimilarity.sketch <- function(f.i, n.groups=2, v=c('clay', 'total_frags_pct')
   cols <- brewer.pal(n=8, name='Set1')
   
   # generate an index to non-R|Cr|Cd horizons
-  filter.idx <- grep('R|Cr|Cd', f.i$hzname, invert=TRUE)
+  filter.idx <- grep('R|Cr|Cd', x$hzname, invert=TRUE)
   
   # establish max depth of eval, using presence of clay
-  max.depth <- max(f.i, 'clay') + 10
+  max.depth <- max(x, 'clay') + 10
   
   # dissimilarity matrix
-  d <- profile_compare(f.i, vars=v, k=0, max_d=max.depth, rescale.result=TRUE, filter=filter.idx)
+  d <- profile_compare(x, vars=v, k=0, max_d=max.depth, rescale.result=TRUE, filter=filter.idx)
   
   # reduce to 2D: fudge 0-distances by adding a little bit
-  s <- isoMDS(d+0.001)
+  s <- isoMDS(d+0.001, trace=FALSE)
   # generate plotting order as Euclidean distance to origin
+  # this is similar to the similarity to the multivariate mean -- "modal concept"
   new.order <- order(sqrt(rowSums(sweep(s$points, MARGIN=2, STATS=c(0, 0), FUN='-')^2)))
-  
+
   # convert distance matrix into dendrogram
   h <- as.hclust(diana(d))
   p <- as.phylo(h)
@@ -31,10 +32,21 @@ dissimilarity.sketch <- function(f.i, n.groups=2, v=c('clay', 'total_frags_pct')
   # cut into groups
   g <- cutree(h, n.groups)
   # merge into site data
-  site(f.i) <- data.frame(pedon_id=names(g), group=g, stringsAsFactors=FALSE)
+  site(x) <- data.frame(pedon_id=names(g), group=g, stringsAsFactors=FALSE)
+  
+  # convert SPC into data.frame
+  df <- as(x, 'data.frame')
+  
+  # return pedon ID + hzname for those values at or outside of 5th--95th pctile range
+  values.to.check <- sapply(v, simplify=FALSE, FUN=function(v.i) {
+    e <- ecdf(df[[v.i]])(df[[v.i]])
+    outlier.idx <- which(e > 0.95 | e < 0.05)
+    paste(df$pedon_id[outlier.idx], df$hzname[outlier.idx], sep='-')
+  })
+  
   
   # subset variable + group for box-whisker plot
-  m <- melt(as(f.i, 'data.frame'), id.vars='group', measure.vars=c(v))
+  m <- melt(df, id.vars='group', measure.vars=c(v))
   m$group <- factor(m$group)
   m$variable <- factor(m$variable)
   
@@ -47,7 +59,7 @@ dissimilarity.sketch <- function(f.i, n.groups=2, v=c('clay', 'total_frags_pct')
   tiplabels(pch=15, col=cols[g])
   
   # box-whisker plot
-  par(mar=c(0.125,0.75,0.25,0.25))
+  par(mar=c(0.125,0.125,0.25,0.25))
   boxplot(value ~ variable, data=m, horizontal=TRUE, las=1, cex.axis=0.75, xlab='Percent', boxwex=0.5, border=grey(0.5), axes=FALSE)
   text(x=tapply(m$value, m$variable, median, na.rm=TRUE), y=(1:2)+0.35, levels(m$variable), cex=1, font=2, pos=4)
   points(m$value, jitter(as.numeric(m$variable)), col=cols[m$group], pch=0)
@@ -55,7 +67,10 @@ dissimilarity.sketch <- function(f.i, n.groups=2, v=c('clay', 'total_frags_pct')
   
   # lower panel: profiles
   par(mar=c(0.25,0.25,0.25,2))
-  plot(f.i, name='hzname', id.style='side', cex.depth.axis=1.125, cex.id=0.8, cex.names=0.65, plot.order=new.order, max.depth=max.depth, axis.line.offset=-3.5)
-  segments(x0=0.5, y0=c(50, 75, 100, 150), x1=length(f.i) + 1.5, y1=c(50, 75, 100, 150), lty=3, col=rgb(0, 0, 0, alpha=0.25))
-  points(x=1:length(f.i), y=rep(-2.5, times=length(f.i)), pch=15, col=cols[g[new.order]], cex=1.5)
+  plot(x, name='hzname', id.style='side', alt.label='taxonname', cex.depth.axis=1.125, cex.id=0.8, cex.names=0.65, plot.order=new.order, max.depth=max.depth, axis.line.offset=-3.5)
+  segments(x0=0.5, y0=c(50, 75, 100, 150), x1=length(x) + 1.5, y1=c(50, 75, 100, 150), lty=3, col=rgb(0, 0, 0, alpha=0.25))
+  points(x=1:length(x), y=rep(-2.5, times=length(x)), pch=15, col=cols[x$group[new.order]], cex=1.5)
+    
+  # return list of values that may need review:
+  return(values.to.check)
 }

176833ed712f3dac23d25623261835605c20922b
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2014-05-30 16:02:15 +0000

    added check for ultrametric trees in dueling.dendrograms

--- R/dueling.dendrograms.R
+++ R/dueling.dendrograms.R
@@ -1,6 +1,10 @@
 # visually compare two related dendrograms
 dueling.dendrograms <- function(p.1, p.2, lab.1='D1', lab.2='D2', cex.nodelabels=0.75, arrow.length=0.05) {
 	
+  # this function only works with ultrametric objects
+  if(!is.ultrametric(p.1) || !is.ultrametric(p.2))
+    stop('both phylo objects must be ultrametric')
+  
 	# setup some geometry for connecting lines / arrows
 	arrow.left <- 0.1
 	arrow.right <- 0.9
@@ -15,7 +19,10 @@ dueling.dendrograms <- function(p.1, p.2, lab.1='D1', lab.2='D2', cex.nodelabels
 
 	# save results of phylo environment
 	p.left <- get("last_plot.phylo", envir = .PlotPhyloEnv)
-
+  
+  # get left-hand IDs
+  p.left.ids <- as.hclust(p.1)$labels
+  
 	# right-hand side dendrogram
 	plot(p.2, cex=cex.nodelabels, font=1, direction='leftwards', no.margin=TRUE, label.offset=0.015)
 	mtext(lab.2, side=3, line=-1.5, font=2, cex=0.75)
@@ -23,6 +30,9 @@ dueling.dendrograms <- function(p.1, p.2, lab.1='D1', lab.2='D2', cex.nodelabels
 	# save results of phylo environment
 	p.right <- get("last_plot.phylo", envir = .PlotPhyloEnv)
 
+	# get right-hand IDs
+	p.right.ids <- as.hclust(p.2)$labels
+  
 	# get ordering of pedon IDs on each side
 	left.new_order <- sapply(1:p.left$Ntip, function(i) which(as.integer(p.left$yy[1:p.left$Ntip]) == i))
 	right.new_order <- sapply(1:p.right$Ntip, function(i) which(as.integer(p.right$yy[1:p.right$Ntip]) == i))

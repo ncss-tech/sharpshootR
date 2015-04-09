7491af7f1da0d66d77a23375424b589b78b6d9e6
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2015-04-09 23:01:03 +0000

    new function for generating a unique ID for line segments based on rounded start/end verticies

---
+++ R/generateLineHash.R
@@ -0,0 +1,22 @@
+generateLineHash <- function(x, precision=-1) {
+  the.lines <- slot(x, 'lines')
+  # sanity check:
+  # (there should only be 1 / Line object)
+  lines.per.Line <- sapply(the.lines, function(i) length(i@Lines))
+  if(any(lines.per.Line > 1))
+    stop('more than 1 line per segment, why?')
+  
+  # unzip lines into single list
+  the.coords <- lapply(the.lines, function(i) slot(slot(i, 'Lines')[[1]], 'coords'))
+  
+  # generate digest from start / end vertices, rounded to defined precision
+  res <- sapply(the.coords, function(i) {
+    n <- nrow(i)
+    start.coord <- i[1, ]
+    end.coord <- i[n, ]
+    hash <- digest(c(round(start.coord, precision), round(end.coord, precision)))
+    return(hash)
+  })
+  
+  return(res)
+}

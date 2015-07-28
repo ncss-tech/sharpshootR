014241630be5399c75332df537642aabd3e11420
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-07-28 17:11:52 +0000

    improvements to aggregate color estimation and plotting functions

--- pkg/sharpshootR/R/aggregateColorPlot.R
+++ pkg/sharpshootR/R/aggregateColorPlot.R
@@ -1,7 +1,7 @@
 
 
 # x: results from aggregateColor()
-aggregateColorPlot <- function(x, label.font=1, label.cex=0.65, buffer.pct=0.02, print.n.hz=FALSE, ...) {
+aggregateColorPlot <- function(x, label.font=1, label.cex=0.65, buffer.pct=0.02, print.n.hz=FALSE, rect.border='black', ...) {
  
   # extract just the scaled data from the results of aggregateColor()
   s.scaled <- x$scaled.data
@@ -10,8 +10,7 @@ aggregateColorPlot <- function(x, label.font=1, label.cex=0.65, buffer.pct=0.02,
   max.plot <- max(sapply(s.scaled, function(i) sum(i$weight)))
   
   # setup plot
-  plot(1,1, type='n', xlim=c(0, max.plot), ylim=c(length(names(s.scaled))+0.5, 0.5), axes=FALSE, ylab='', xlab='', ...)
-  box()
+  plot(1,1, type='n', xlim=c(0, max.plot), ylim=c(length(names(s.scaled))+0.5, 0.5), axes=FALSE, ylab='', xlab='Cumulative Proportion', ...)
   # iterate over horizons
   for(i in seq_along(names(s.scaled))) {
     s.i <- s.scaled[[i]]
@@ -24,7 +23,7 @@ aggregateColorPlot <- function(x, label.font=1, label.cex=0.65, buffer.pct=0.02,
       x.right <- c(x.left[-1], x.left[last.weight] + s.i$weight[last.weight])
       
       # plot rectanges from vectorized coordinates / colors
-      rect(xleft = x.left, ybottom = i-0.5, xright = x.right, ytop = i+0.5, col = s.i$soil_color)
+      rect(xleft=x.left, ybottom=i-0.5, xright=x.right, ytop=i+0.5, col=s.i$soil_color, border=rect.border)
       
       # compute center point for color labels
       centers <- (x.right + x.left) / 2
@@ -35,8 +34,17 @@ aggregateColorPlot <- function(x, label.font=1, label.cex=0.65, buffer.pct=0.02,
       else
         color.labels <- s.i$munsell
       
-      # determine if there is enough room to label colors: weight of current color - some % of max weight (1)
-      print.label <- which(abs(strheight(color.labels, cex=label.cex, font=label.font)) < (s.i$weight - buffer.pct) )
+      # determine if there is enough room to label colors: some % of visible space on plot
+      # first, get the plot aspect ratio
+      plot.w <- par("pin")[1]/diff(par("usr")[1:2])
+      plot.h <- par("pin")[2]/diff(par("usr")[3:4])
+      plot.asp <- abs(plot.w / plot.h)
+      # get text heigths, as we will be printing labels at 90 deg
+      text.heights <- abs(strheight(color.labels, cex=label.cex, font=label.font))
+      # convert text heights into equivelent widths
+      text.heights <- text.heights / plot.asp
+      # compare re-scaled text heights with rectangle widths (weights) + some buffer
+      print.label <- which(text.heights < (s.i$weight - buffer.pct) )
       
       # print labels
       if(length(print.label) > 0)
@@ -44,6 +52,8 @@ aggregateColorPlot <- function(x, label.font=1, label.cex=0.65, buffer.pct=0.02,
     }
   }
   
-  axis(2, at = seq_along(names(s.scaled)), labels = names(s.scaled), las=2)
+  # add axes
+  axis(1, at=round(seq(0, 1, length.out = 11), 2))
+  axis(2, at = seq_along(names(s.scaled)), labels = names(s.scaled), las=2, tick=FALSE, font=2, hadj=0, line=-0.5, cex.axis=1.125)
   
 }

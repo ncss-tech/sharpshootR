48ce37136489340ae58651a0624f506354b054c3
Dylan Beaudette <dylan.beaudette@gmail.com>
2016-08-25 14:45:38 -0700

    working version + docs

--- R/CDEC_StationInfo.R
+++ R/CDEC_StationInfo.R
@@ -4,13 +4,13 @@
 CDEC_StationInfo <- function(s) {
   # check for required packages
   if(!requireNamespace('rvest', quietly = TRUE))
-    stop('please install the `rgeos` package', call.=FALSE)
+    stop('please install the `rvest` package', call.=FALSE)
   
   u <- paste0("http://cdec.water.ca.gov/cgi-progs/queryCSV?station_id=", s)
-  h <- read_html(u)
-  hn <- html_nodes(h, "table")
+  h <- rvest::read_html(u)
+  hn <- rvest::html_nodes(h, "table")
   if(length(as.list(hn)) > 0) {
-    ht <- html_table(hn[[1]])
+    ht <- rvest::html_table(hn[[1]])
     names(ht) <- c('sensor', 'sensor_details', 'interval', 'period_of_record')
     return(ht)
   } else {

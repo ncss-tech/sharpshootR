b284b735f42dc5851d118318ccbdb5cd9bfc14c8
Dylan Beaudette <dylan.beaudette@gmail.com>
2016-08-27 11:54:10 -0700

    bug fix  related to import of suggested package::function

--- R/CDEC_StationInfo.R
+++ R/CDEC_StationInfo.R
@@ -3,11 +3,11 @@
 # s: CDEC station ID
 CDEC_StationInfo <- function(s) {
   # check for required packages
-  if(!requireNamespace('rvest', quietly = TRUE))
+  if(!requireNamespace('rvest', quietly = TRUE) | !requireNamespace('xml2', quietly = TRUE))
     stop('please install the `rvest` package', call.=FALSE)
   
   u <- paste0("http://cdec.water.ca.gov/cgi-progs/queryCSV?station_id=", s)
-  h <- rvest::read_html(u)
+  h <- xml2::read_html(u)
   hn <- rvest::html_nodes(h, "table")
   if(length(as.list(hn)) > 0) {
     ht <- rvest::html_table(hn[[1]])

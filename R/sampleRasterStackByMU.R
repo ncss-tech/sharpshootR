cd89f1bf6105aaa75edebca270af5f7627976af2
Dylan Beaudette <dylan.beaudette@gmail.com>
2016-10-28 13:45:54 -0700

    leaving some debugging outputp

--- R/sampleRasterStackByMU.R
+++ R/sampleRasterStackByMU.R
@@ -179,7 +179,7 @@ sampleRasterStackByMU <- function(mu, mu.set, mu.col, raster.list, pts.per.acre,
         
         ## TODO: this may be far too slow
         if(estimateEffectiveSampleSize) {
-          # message(paste0('   Estimating effective sample size: ', i.name))
+          message(paste0('   Estimating effective sample size: ', i.name))
           
           # compute within each polygon: slightly faster
           ss <- list()

eabfe103c504f7d3ca8909c892dbd067b9280029
Dylan Beaudette <dylan.beaudette@gmail.com>
2016-12-23 11:35:30 -0800

    combining functionality

--- R/samplingStability.R
+++ R/samplingStability.R
@@ -12,6 +12,26 @@
 # result: data.frame with median stability values as percentage of population median, range:{0,1}
 samplingStability <- function(mu, r, n.set=c(0.01, 0.1, 0.5, 1, 2), n.reps=10, p.id='pID') {
   
+  # summary by quantiles 
+  .summary <- function(i, p=c(0, 0.05, 0.25, 0.5, 0.75, 0.95, 1)) {
+    # remove NA
+    v <- na.omit(i)
+    # compute quantiles
+    q <- quantile(v, probs=p)
+    res <- data.frame(t(q))
+    
+    # assign reasonable names (quantiles)
+    if(nrow(res) > 0) {
+      names(res) <- c(paste0('Q', p * 100))
+      # compute size
+      res$n <- length(i)
+      return(res)
+    }
+    else
+      return(NULL)
+  }
+  
+  
   # sample / raster overlay
   .sample <- function(mu, r, n, p.id) {
     s <- constantDensitySampling(mu, n.pts.per.ac=n, min.samples=1, polygon.id=p.id)
@@ -27,32 +47,60 @@ samplingStability <- function(mu, r, n.set=c(0.01, 0.1, 0.5, 1, 2), n.reps=10, p
     return(e)
   }
   
-  # get the "population median"
-  pop.median <- median(.population(mu, r), na.rm = TRUE)
+  # init a list to store tabular summaries
+  
+  # get population and median
+  pop <- .population(mu, r)
+  pop.median <- median(pop, na.rm = TRUE)
+  n.pixels <- length(pop)
   
   # iterate over sampling densities in our example
+  # this list will be used for two things:
+  #    1. table of quantiles, number of samples, and pct of pixels
+  #    2. medians extracted for stability index
   r.sample <- list()
   for(i in n.set) {
-    # estimate median value n.reps times via sampling
-    r.sample[[as.character(i)]] <- replicate(n.reps, median(.sample(mu, r, i, p.id), na.rm = TRUE))
+    # estimate select quantiles and sample size over replications
+    # result is a list
+    s.i <- replicate(n.reps, .summary(.sample(mu, r, i, p.id)), simplify = FALSE)
+    # convert to DF and store
+    r.sample[[as.character(i)]] <- ldply(s.i)
   }
   
+  ## step 1: table of quantiles
   # add sampling units
   names(r.sample) <- paste0(names(r.sample), ' pts/ac')
   
+  # create table of quantiles using mean of replications
+  summary.table <- lapply(r.sample, colMeans)
+  
+  # add population
+  summary.table[['population']] <- unlist(.summary(pop))
+  
+  # convert summary table to DF
+  summary.table <- ldply(summary.table)
+  
+  # convert sample size to integer
+  summary.table$n <- round(summary.table$n)
+  
+  # compute pixels of population sampled
+  summary.table$`percent pixels sampled` <- (summary.table$n / summary.table$n[summary.table$.id == 'population']) * 100
+  
+  ## step 2: stability of median
   # compute 5th--95th pctile interval across all samples
-  res <- ldply(r.sample, function(i) {
-    # interval
-    qq <- quantile(i, probs=c(0.05, 0.95))
+  stability <- ldply(r.sample, function(i) {
+    # get interval
+    qq <- quantile(i[['Q50']], probs=c(0.05, 0.95))
     qq <- abs(diff(qq))
     # scale interval size relative to population median
     return(qq / pop.median)
   })
   
   # fix names
-  names(res) <- c('sampling.density', 'stability')
+  names(stability) <- c('sampling.density', 'stability')
+  names(summary.table)[1] <- 'sampling.density'
   
-  return(res)
+  return(list(summary.table=summary.table, stability=stability))
 }
 
 

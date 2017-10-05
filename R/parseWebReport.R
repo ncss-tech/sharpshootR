0d5d8f978bce3b68b464f1eef1d367b42c2d2e83
Dylan Beaudette <dylan.beaudette@gmail.com>
2017-10-05 15:14:26 -0700

    notes for later

--- R/parseWebReport.R
+++ R/parseWebReport.R
@@ -1,5 +1,7 @@
 
 # requires rvest
+# note: get argument names from report HTML source
+#
 # examples:
 # url = 'https://nasis.sc.egov.usda.gov/NasisReportsWebSite/limsreport.aspx?report_name=WEB-PROJECT_MUKEY_BY_GOAL_YEAR'
 # args = list(msso='2-MIN', fy='2018', asym='%', proj='0')
@@ -25,6 +27,8 @@ parseWebReport <- function(url, args, index=1) {
     # extract tables if index if specified
     d <- rvest::html_table(x, header=TRUE)[[index]]
   }
+  
+  # TODO: col names aren't legal data.frame names
   
   # done
   return(d)

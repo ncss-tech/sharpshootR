40a429c8c5348125d7171876eb206741b990c99c
Dylan Beaudette <debeaudette@ucdavis.edu>
2018-09-18 15:57:57 -0700

    CDEC API change, crumbles...

--- R/CDEC_StationInfo.R
+++ R/CDEC_StationInfo.R
@@ -6,14 +6,27 @@ CDEC_StationInfo <- function(s) {
   if(!requireNamespace('rvest', quietly = TRUE) | !requireNamespace('xml2', quietly = TRUE))
     stop('please install the `rvest` package', call.=FALSE)
   
-  u <- paste0("http://cdec.water.ca.gov/cgi-progs/queryCSV?station_id=", s)
+  u <- sprintf("http://cdec.water.ca.gov/dynamicapp/staMeta?station_id=%s", s)
   h <- xml2::read_html(u)
   hn <- rvest::html_nodes(h, "table")
+  
+  # make sure there are some results to process, there should be 3 tables
   if(length(as.list(hn)) > 0) {
-    ht <- rvest::html_table(hn[[1]])
-    names(ht) <- c('sensor', 'sensor_details', 'interval', 'period_of_record')
-    return(ht)
+    # site, poorly formatted
+    site.meta <- rvest::html_table(hn[[1]])
+    
+    # sensors, can be converted into data.frame
+    sensor.meta <- rvest::html_table(hn[[2]])
+    names(sensor.meta) <- c('sensor_details', 'sensor', 'interval', 'sensor_name', 'collection_method', 'period_of_record')
+    
+    # notes, may be missing
+    # site.notes <- rvest::html_table(hn[[3]])
+    
+    ## TODO: return a list with nicely-formatted site, sensor, notes
+    return(sensor.meta)
+    
   } else {
+    
     message('no station found')
     return(NULL)
   }

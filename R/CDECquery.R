6d09d7a54f4bcf082544de98ae7fb34c4cfa5fee
Dylan Beaudette <debeaudette@ucdavis.edu>
2018-09-18 23:57:41 -0700

    fixes for #13

--- R/CDECquery.R
+++ R/CDECquery.R
@@ -7,15 +7,14 @@ CDECquery <- function(id, sensor, interval='D', start, end) {
   if(missing(id) | missing(sensor) | missing(start) | missing(end))
     stop('missing arguments', call.=FALSE)
   
-  ## 2018-09-18: new CDEC API, multiple sensors can be specified in one request 
+  ## 2018-09-18: new CDEC API, 
+  ## multiple stations and sensors can be specified in one request
   ## e.g.: &SensorNums=194,197
-  ## seems slower than before
-  ##
-  ## TODO: currently broken, why?
   # construct the URL for the DWR website  
   u <- paste0(
-    'http://cdec.water.ca.gov/dynamicapp/req/CSVDataServlet?Stations=', id, 
-    '&SensorNums=', sensor, 
+    'http://cdec.water.ca.gov/dynamicapp/req/CSVDataServlet?',
+    '&Stations=', paste(id, collapse = ','), 
+    '&SensorNums=', paste(sensor, collapse = ','), 
     '&dur_code=', interval, 
     '&Start=', start,
     '&End=', end)
@@ -28,34 +27,36 @@ CDECquery <- function(id, sensor, interval='D', start, end) {
   suppressWarnings(download.file(url=u, destfile=tf, quiet=TRUE))
   
   # try to parse CSV
-  d <- read.csv(file=tf, header=TRUE, na.strings='', stringsAsFactors=FALSE)
-  d <- try(read.csv(file=tf, header=TRUE, na.strings='', stringsAsFactors=FALSE),  silent=TRUE)
+  # 2018-09-18: CDEC API returns bogus CSV file
+  # https://github.com/ncss-tech/sharpshootR/issues/13
+  # read without header and skip first row
+  # NA encoded as '---'
+  # manually specify column classes so that time 0000 (HHMM) is corectly encoded
+  CDEC.columns <- c('station_id', 'dur_code', 'sensor_num', 'sensor_type', 'date', 'time', 'value', 'flag', 'units')
+  CDEC.column.class <- c('character', 'character', 'integer', 'character', 'character', 'character', 'numeric', 'character', 'character')
+  d <- try(read.csv(file=tf, header=FALSE, skip=1, na.strings='---', colClasses = CDEC.column.class))
+  
+  # add column names
+  names(d) <- CDEC.columns
   
   # catch errors
   if(class(d) == 'try-error') {
-    ref.url <- paste0('invalid URL; see ','http://cdec.water.ca.gov/cgi-progs/queryCSV?station_id=', id)
-    stop(ref.url, call.=FALSE)
+    stop(sprintf('invalid URL: %s', u), call.=FALSE)
   }
   
-  # no data available
-  if(nrow(d) == 0)
-    stop('query returned no data', call.=FALSE)
-  
+  # no data available, play nicely with block of requestes
+  if(nrow(d) < 1) {
+    message('query returned no data', call.=FALSE)
+    return(NULL)
+  }
+    
   # convert date/time to R-friendly format
-  d$datetime <- as.POSIXct(paste(d[[1]], d[[2]]), format="%Y%m%d %H%M")
-  
-  # strip-out first two columns
-  d[[1]] <- NULL # date part
-  d[[1]] <- NULL # time part
-  
-  # note: d[[1]] will now contain the requested data, but likely with a nasty label
-  # relable here:
-  names(d)[1] <- 'value'
+  d$datetime <- as.POSIXct(paste(d$date, d$time), format="%Y%m%d %H%M")
   
   # extract the year and month for reporting ease later
   d$year <- as.numeric(format(d$datetime, "%Y"))
   d$month <- factor(format(d$datetime, '%B'), levels=c('January','February','March','April','May','June','July','August','September','October','November','December'))
   
-  # return the result in a more useful order
-  return(d[, c('datetime', 'year', 'month', 'value')])
+  # 2018-09-18: more data returned by the API, so makes sense to include additional columns
+  return(d)
 }

75e7f249c1a04b07b5dd3c7ef6606646a5c290ad
Dylan Beaudette <debeaudette@ucdavis.edu>
2019-07-15 15:58:38 -0700

    attempting relative spacing

--- R/transect-functions.R
+++ R/transect-functions.R
@@ -18,7 +18,7 @@ dist.along.grad <- function(coords, var, grad.scaled.min, grad.scaled.max) {
 }
 
 # plot a transect with profiles below
-plotTransect <- function(s, grad.var.name, transect.col='RoyalBlue', tick.number=7, y.offset=100, scaling.factor=0.5, distance.axis.title='Distance Along Transect (km)', crs=NULL, grad.axis.title=NULL, dist.scaling.factor=1000, ...){
+plotTransect <- function(s, grad.var.name, transect.col='RoyalBlue', tick.number=7, y.offset=100, scaling.factor=0.5, distance.axis.title='Distance Along Transect (km)', crs=NULL, grad.axis.title=NULL, dist.scaling.factor=1000, spacing='regular', ...){
   
   # internal offsets
   
@@ -38,15 +38,32 @@ plotTransect <- function(s, grad.var.name, transect.col='RoyalBlue', tick.number
   # create transect
   transect <- dist.along.grad(coords, site(s)[[grad.var.name]], grad.scaled.min=0, grad.scaled.max=y.offset-15)
   
+  print(transect)
+  
   # use a linear model to translate original gradient -> scaled gradient 
   l <- lm(scaled.grad ~ variable, data=transect)
   
   # establish gradient axis tick marks
   axis.labels <- pretty(transect$variable, n=tick.number)
   axis.pos <- round(predict(l, data.frame(variable=axis.labels)))
+  
+  # spacing style
+  if(spacing == 'regular') {
+    # this var will be used to position arrows / segments
+    x.pos <- 1:length(s)
+    # standard, regular spacing along x-asis
+    plot(s, plot.order=transect$grad.order, y.offset=y.offset, scaling.factor=scaling.factor, id.style='side', ...)
     
-  # setup basic plot
-  plot(s, plot.order=transect$grad.order, y.offset=y.offset, scaling.factor=scaling.factor, id.style='side', ...)
+  } else if(spacing == 'relative') {
+    # this var will be used to position sketches, arrows, segments
+    x.pos <- transect$scaled.distance
+    # plot sketches according to relative spacing along x-axis
+    plot(s, plot.order=transect$grad.order, y.offset=y.offset, scaling.factor=scaling.factor, id.style='side', relative.pos=x.pos, ...)
+  } else {
+    stop('spacing must be specified as `regular` or `relative`', call. = FALSE)
+  }
+  
+  
   
   # add gridlines
   abline(h=axis.pos, lty=2, col='grey')
@@ -59,14 +76,14 @@ plotTransect <- function(s, grad.var.name, transect.col='RoyalBlue', tick.number
     mtext(grad.axis.title, at=median(transect$scaled.grad), side=2, line=2.5, font=2, cex=0.75)
   
   # add distance along gradient
-  axis(side=1, at=1:length(s), labels=round(transect$distance/dist.scaling.factor), cex.axis=0.75)
+  axis(side=1, at=x.pos, labels=round(transect$distance/dist.scaling.factor), cex.axis=0.75)
   mtext(distance.axis.title, side=1, line=2, font=2, cex=0.75)
   
   # link gradient points to profiles
-  segments(x0=1:length(s), y0=y.offset-15, x1=transect$scaled.distance, y1=transect$scaled.grad, lty=1)
+  segments(x0=x.pos, y0=y.offset-15, x1=transect$scaled.distance, y1=transect$scaled.grad, lty=1)
   
   # add arrows
-  arrows(x0=1:length(s), y0=y.offset-15, x1=1:length(s), y1=y.offset-2, length = 0.1, code=2, lty=1)
+  arrows(x0=x.pos, y0=y.offset-15, x1=x.pos, y1=y.offset-2, length = 0.1, code=2, lty=1)
   
   # add gradient line
   lines(transect$scaled.distance, transect$scaled.grad, lwd=2, col=transect.col)

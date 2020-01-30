98e3b7feb092508cd9904e3a30af44b898e7d0a7
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-01-30 11:35:05 -0800

    notes, parallel processing isnt really working

--- misc/profile-constantDensitySampling.R
+++ misc/profile-constantDensitySampling.R
@@ -8,7 +8,7 @@ library(rgdal)
 library(parallel)
 
 # setup
-r <- raster('E:/gis_data/CONUS-forms-DEB.tif')
+r <- raster('E:/gis_data/CONUS/CONUS-forms-DEB.tif')
 # r <- raster('E:/gis_data/NASS/2016_30m_cdls.img')
 
 # get several polygons
@@ -19,20 +19,29 @@ s$pID <- 1:nrow(s)
 nrow(s)
 
 # parallel / serial comparison
+
+# parallel 2x slower
 system.time(ss <- constantDensitySampling(s, n.pts.per.ac=0.01, parallel = FALSE))
 system.time(ss <- constantDensitySampling(s, n.pts.per.ac=0.01, parallel = TRUE))
 
-# hmm... about equal performance
+# equal performance ~ 80 seconds
 system.time(ss <- constantDensitySampling(s, n.pts.per.ac=0.1, parallel = FALSE))
 system.time(ss <- constantDensitySampling(s, n.pts.per.ac=0.1, parallel = TRUE))
+
+# 
+# 642 seconds
+system.time(ss <- constantDensitySampling(s, n.pts.per.ac=1, parallel = FALSE))
+# 790 seconds
+system.time(ss <- constantDensitySampling(s, n.pts.per.ac=1, parallel = TRUE))
+
 
 
 ## profile
 ## http://adv-r.had.co.nz/Profiling.html#measure-perf
 ## https://cran.r-project.org/web/packages/proftools/index.html
 # profile
-pd.1 <- profileExpr(ss <- constantDensitySampling(s, n.pts.per.ac=0.01, parallel = TRUE))
-pd.2 <- profileExpr(ss <- constantDensitySampling(s, n.pts.per.ac=0.01, parallel = FALSE))
+pd.1 <- profileExpr(ss <- constantDensitySampling(s, n.pts.per.ac=0.1, parallel = TRUE))
+pd.2 <- profileExpr(ss <- constantDensitySampling(s, n.pts.per.ac=0.1, parallel = FALSE))
 
 hotPaths(pd.1)
 hotPaths(pd.2)
@@ -58,7 +67,9 @@ round(prop.table(table(e)), 2)
 ## try a large number of polygons
 s <- readOGR(dsn='e:/gis_data/ca630/FG_CA630_OFFICIAL.gdb', layer='ca630_a')
 s$pID <- 1:nrow(s)
+# 
 system.time(ss <- constantDensitySampling(s, n.pts.per.ac=0.5, parallel = FALSE))
+# 
 system.time(ss <- constantDensitySampling(s, n.pts.per.ac=0.5, parallel = TRUE))
 
 

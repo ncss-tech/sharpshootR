3708817d2ba3f68659b0b99ea141a4cd9df944f1
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-09-11 22:22:59 -0700

    simple test for circular uniformity, related to mean resultant length as symbolized with arrows

--- R/aspect.plot.R
+++ R/aspect.plot.R
@@ -33,6 +33,11 @@ aspect.plot <- function(p, q=c(0.05, 0.5, 0.95), p.bins=60, p.bw=30, stack=TRUE,
 	# add title
 	text(0, -0.25, plot.title, cex=0.85, font=2)
   
-  # invisibly return stats
+	## TODO: this is kind of noisy, consider adding to the plot or output
+	# compare relative to uniform circular distribution
+	rt <- rayleigh.test(c.p)
+	print(rt)
+	
+	# invisibly return stats
   invisible(q.p)
 }

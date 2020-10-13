cf38d44c738af3539e95054c94e8a3ebae704c47
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-10-13 11:53:30 -0700

    cleanup, code ready for winbuilder

---
+++ R/data-documentation.R
@@ -0,0 +1,37 @@
+##
+## document data here
+## don't forget: @usage data(XXX)
+
+
+#'
+#' @title Table 5.2 from Hole and Campbell, 1985.
+#' @description An adjacency matrix describing shared soil map boundary segments from the Soil Survey of Shawnee county, KS. This is table 5.2 from Hole and Campbell, 1985.
+#' 
+#' @keywords datasets
+#'
+#' @usage data(table5.2)
+#'
+#' @references Hole, F.D. and J.B. Campbell. Soil Landscape Analysis. Rowman and Allanheld, 1985. 
+#' 
+#' @examples
+#' 
+#' data("table5.2")
+#' 
+#' if(requireNamespace("igraph")) {
+#'   
+#'   # note special incantation to get the "correct" graph structure
+#'   g <- igraph::graph_from_adjacency_matrix(table5.2, mode = 'upper', diag = FALSE, weighted = TRUE)
+#'   
+#'   # visualize
+#'   par(mar = c(0,0,0,0))
+#'   plot(g)
+#'   
+#'   plot(g, vertex.size = sqrt(igraph::degree(g) * 25), vertex.label.family = 'sans')
+#'   
+#'   # find communities
+#'   cm <- igraph::cluster_walktrap(g)
+#'   plot(cm, g, vertex.label.family = 'sans')
+#'   
+#' }
+#'
+"table5.2"
--- misc/make-hole-campbell-adj-mat.R
+++ misc/make-hole-campbell-adj-mat.R
@@ -1,23 +1,38 @@
 library(igraph)
 
+# table 5.2 from
+# Hole, F.D. and J.B. Campbell. Soil Landscape Analysis. Rowman Allanheld, 1985.
+
+# hand-keyed
 x <- read.table('table-5.2.txt', sep = ',')
+
+# check: OK
 str(x)
 x
 
 nm <- c('An', 'Bk', 'Br', 'Dm', 'Dw', 'La', 'Ld', 'Me', 'Mp', 'Pa', 'Re', 'Sk', 'Sv', 'Vn', 'Kb', 'El', 'Mn', 'Em')
 
+# DF -> matrix
 x <- as.matrix(x)
 dimnames(x) <- list(nm, nm)
 
+# check: OK
 x
 
+# note special incantation to get the "correct" graph structure
 g <- graph_from_adjacency_matrix(x, mode = 'upper', diag = FALSE, weighted = TRUE)
-cm <- cluster_walktrap(g)
 
+# does it make sense?
+# seems to
 par(mar = c(0,0,0,0))
 plot(g)
 
 plot(g, vertex.size = sqrt(degree(g) * 25), vertex.label.family = 'sans')
 
-
+# find communities
+cm <- cluster_walktrap(g)
 plot(cm, g, vertex.label.family = 'sans')
+
+# rename / save
+table5.2 <- x
+save(table5.2, file = '../data/table5.2.rda')

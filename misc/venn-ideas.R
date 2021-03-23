0a8bf9062015732939c15d995d3248f7a4c0a7b1
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-03-23 15:46:50 -0700

    more examples

--- misc/venn-ideas.R
+++ misc/venn-ideas.R
@@ -34,12 +34,39 @@ colorMixtureVenn(chips, labels = FALSE)
 
 colorMixtureVenn(chips, ellipse = TRUE)
 
+colorMixtureVenn(chips, ellipse = TRUE, mixingMethod = 'estimate')
+
 par(bg = 'black', fg = 'white')
 colorMixtureVenn(chips, ellipse = TRUE)
 
 
+par(mfcol = c(1,2))
 chips <- c('10YR 8/1', '2.5YR 3/6', '10YR 2/2')
 colorMixtureVenn(chips)
+title('Simulation of Subtractive Mixture', line = -4)
+
+colorMixtureVenn(chips, mixingMethod = 'estimate')
+title('Weighted Mean CIELAB', line = -4)
+
+
+chips <- c('10YR 10/1', '2.5YR 3/6', '10YR 1/1')
+colorMixtureVenn(chips)
+title('Simulation of Subtractive Mixture', line = -4)
+
+colorMixtureVenn(chips, mixingMethod = 'estimate')
+title('Weighted Mean CIELAB', line = -4)
+
+
+
+par(mfcol = c(1,2))
+chips <- c('10YR 8/1', '5Y 8/8', '5B 4/8')
+colorMixtureVenn(chips)
+title('Simulation of Subtractive Mixture', line = -4)
+
+colorMixtureVenn(chips, mixingMethod = 'estimate')
+title('Weighted Mean CIELAB', line = -4)
+
+
 
 
 chips <- c('10YR 8/1', '2.5YR 3/6', '10YR 8/10', '5G 4/8')
@@ -48,8 +75,8 @@ colorMixtureVenn(chips, ellipse = TRUE)
 chips <- c('2.5YR 8/2', '2.5B 3/6', '2.5G 4/4', '2.5R 4/8', '2.5Y 6/10')
 colorMixtureVenn(chips, ellipse = TRUE)
 
-chips <- c('10YR 8/2', '10YR 8/2', '5B 4/8', '5B 4/8')
-colorMixtureVenn(chips)
+chips <- c('10YR 8/2', '5G 4/8', '2.5Y 8/2', '5R 4/8')
+colorMixtureVenn(chips, ellipse = TRUE)
 
 library(soilDB)
 x <- fetchOSD('musick')

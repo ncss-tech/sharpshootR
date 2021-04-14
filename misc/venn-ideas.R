27b617b0cbd8bb721d127cc7388ad4138dfcda16
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-04-13 23:12:33 -0700

    cleanup

--- misc/venn-ideas.R
+++ misc/venn-ideas.R
@@ -28,8 +28,8 @@ chips <- c('10YR 8/2', '10R 4/8', '5Y 8/8', '5B 4/8')
 # # reference diagram
 # venn(4, zcolor = 'bw', box = FALSE, ellipse = TRUE, ilabels = TRUE)
 
-
 colorMixtureVenn(chips)
+colorMixtureVenn(chips, mixingMethod = 'exact')
 colorMixtureVenn(chips, labels = FALSE)
 
 colorMixtureVenn(chips, ellipse = TRUE)
@@ -41,7 +41,9 @@ colorMixtureVenn(chips, ellipse = TRUE, mixingMethod = 'estimate')
 
 par(mfcol = c(1,2))
 colorMixtureVenn(chips, ellipse = TRUE, mixingMethod = 'reference')
+title('Reference Spectra', line = -3)
 colorMixtureVenn(chips, ellipse = TRUE, mixingMethod = 'exact')
+title('Exact', line = -3)
 
 mixMunsell(c('10R 4/8', '5B 4/8'), mixingMethod = 'reference')
 mixMunsell(c('10R 4/8', '5B 4/8'), mixingMethod = 'exact')
@@ -54,10 +56,10 @@ par(mfcol = c(1,2))
 chips <- c('10YR 8/1', '2.5YR 3/6', '10YR 2/2')
 
 colorMixtureVenn(chips, mixingMethod = 'reference')
-title('Simulation (Reference)', line = -4)
+title('Reference', line = -3)
 
 colorMixtureVenn(chips, mixingMethod = 'exact')
-title('Simulation (Exact)', line = -4)
+title('Exact', line = -3)
 
 
 colorMixtureVenn(chips)
@@ -81,33 +83,33 @@ par(mfcol = c(1,2))
 chips <- c('10YR 8/1', '5Y 8/8', '5B 4/8')
 
 colorMixtureVenn(chips, mixingMethod = 'reference')
-title('Simulation of Subtractive Mixture\nReference', line = -4)
+title('Simulation of Subtractive Mixture\nReference', line = -3)
 
 colorMixtureVenn(chips, mixingMethod = 'exact')
-title('Simulation of Subtractive Mixture\nExact', line = -4)
+title('Simulation of Subtractive Mixture\nExact', line = -3)
 
 colorMixtureVenn(chips, mixingMethod = 'exact')
-title('Simulation of Subtractive Mixture\nExact', line = -4)
+title('Simulation of Subtractive Mixture\nExact', line = -3)
 
 colorMixtureVenn(chips, mixingMethod = 'estimate')
-title('Weighted Mean CIELAB', line = -4)
+title('Weighted Mean CIELAB', line = -3)
 
 
 
 
 chips <- c('10YR 8/1', '2.5YR 3/6', '10YR 8/10', '5G 4/8')
-colorMixtureVenn(chips, ellipse = TRUE)
+colorMixtureVenn(chips, ellipse = TRUE, mixingMethod = 'exact')
 
 chips <- c('2.5YR 8/2', '2.5B 3/6', '2.5G 4/4', '2.5R 4/8', '2.5Y 6/10')
-colorMixtureVenn(chips, ellipse = TRUE)
+colorMixtureVenn(chips, ellipse = TRUE, mixingMethod = 'exact')
 
 chips <- c('10YR 8/2', '5G 4/8', '2.5Y 8/2', '5R 4/8')
-colorMixtureVenn(chips, ellipse = TRUE)
+colorMixtureVenn(chips, ellipse = TRUE, mixingMethod = 'exact')
 
 library(soilDB)
 x <- fetchOSD('musick')
 
 chips <- unique(sprintf("%s %s/%s", x$hue, x$value, x$chroma))
 
-colorMixtureVenn(chips[1:5])
-
+colorMixtureVenn(chips[1:5], mixingMethod = 'exact', ellipse = TRUE, labels = FALSE)
+# colorMixtureVenn(chips[1:5], mixingMethod = 'reference')

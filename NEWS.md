aff84b3f63b4a21c55601a493d0221c5da7ef6ef
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-04-29 21:23:25 -0700

    trying for a CRAN release

--- NEWS.md
+++ NEWS.md
@@ -1,9 +1,8 @@
-# sharpshootR 1.8.1 (2021-04-27) 
+# sharpshootR 1.8.1 (2021-04-29) 
  * bugfixes in `plotWB()`
  * new function `plotWB_lines()` c/o J.M. Skovlin
  * `monthlyWB` now includes `AWC` used to init model as an attribute in return value
  * `prepare_SSURGO_hydro_data()` now returns drainage class
 
 # sharpshootR 1.8 (2021-04-22)
- * CRAN release
  * added `simpleWB`, `dailyWB`, and `dailyWB_SSURGO`
--- R/plotSoilRelationChordGraph.R
+++ R/plotSoilRelationChordGraph.R
@@ -1,10 +1,32 @@
 
+#' @title Visualize Soil Relationships via Chord Diagram
+#'
+#' @param m an adjacency matrix, no `NA` allowed
+#' @param s soil of interest, must exist in the column or row names of `m`
+#' @param mult multiplier used to re-scale data in `m` associated with `s`
+#' @param base.color color for all soils other than `s` and 1st and 2nd most commonly co-occurring soils
+#' @param highlight.colors vector of 3 colors: soil of interest, 1st most common, 2nd most common
+#' @param add.legend `logical`, add a legend
+#' @param ... additional arguments passed to `circlize::chordDiagramFromMatrix`
+#'
+#' @details This function is experimental. Documentation pending. See \url{http://jokergoo.github.io/circlize/} for ideas.
+#' 
+#' @author D.E. Beaudette
+#' @keywords hplots
+#' 
 #' @return nothing, function is called to generate graphical output
-
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' data(amador)
+#' m <- component.adj.matrix(amador)
+#' plotSoilRelationChordGraph(m, 'amador')
+#' }
+#' 
 plotSoilRelationChordGraph <- function(m, s, mult=2, base.color='grey', highlight.colors=c('RoyalBlue', 'DarkOrange', 'DarkGreen'), add.legend=TRUE, ...) {
   
-  # check for package
-  # need rgdal
+  # must have this package
   if(!requireNamespace('circlize'))
     stop('Transformation of coordinates requires the `circlize` package.', call.=FALSE)
   
@@ -19,7 +41,7 @@ plotSoilRelationChordGraph <- function(m, s, mult=2, base.color='grey', highligh
   
   # replace values of "1" (two soils that only occur together) with the min non-zero value
   m[m == 1] <- min(m[m > 0])
-  # rescale adj. matrix based on number of co-occuring soils
+  # rescale adj. matrix based on number of co-occurring soils
   m.stats <- apply(m, 1, function(i){length(which(i > 0))})
   m <- sweep(m, MARGIN = 1, STATS = m.stats, FUN = '*')
   # increase value of "central" soil
@@ -27,7 +49,7 @@ plotSoilRelationChordGraph <- function(m, s, mult=2, base.color='grey', highligh
   
   ###
   
-  # locate top n commonly co-occuring soils
+  # locate top n commonly co-occurring soils
   o <- order(m[s, ], decreasing = TRUE)
   s2 <- dn[[1]][o[1]]
   s3 <- dn[[1]][o[2]]
@@ -35,7 +57,7 @@ plotSoilRelationChordGraph <- function(m, s, mult=2, base.color='grey', highligh
   # generate data.frame of all possible connection and fill with single color
   cols <- expand.grid(dn[[1]], dn[[2]], base.color, stringsAsFactors = FALSE)
   
-  # locate connections to central soil and n most common co-occuring soils
+  # locate connections to central soil and n most common co-occurring soils
   s.idx <- c(which(cols[, 1] == s), which(cols[, 2] == s))
   s2.idx <- which(cols[, 1] == s & cols[, 2] == s2)
   s3.idx <- which(cols[, 1] == s & cols[, 2] == s3)
@@ -56,6 +78,12 @@ plotSoilRelationChordGraph <- function(m, s, mult=2, base.color='grey', highligh
   
   # optional legend
   if(add.legend) {
-    legend('topleft', legend=c('Queried', '1st', '2nd', 'Others'), pt.bg=c(highlight.colors, base.color), pch=22, pt.cex=2)
+    legend(
+      'topleft', 
+      legend = c('Queried', '1st', '2nd', 'Others'), 
+      pt.bg = c(highlight.colors, base.color), 
+      pch = 22, 
+      pt.cex = 2
+    )
   }
 }

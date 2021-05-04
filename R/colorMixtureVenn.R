0f0db33453e484e9a3a6427c6a27b60fd2a31101
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-05-04 11:36:12 -0700

    too many slow running examples, save for a tutorial

--- R/colorMixtureVenn.R
+++ R/colorMixtureVenn.R
@@ -30,15 +30,6 @@
 #' colorMixtureVenn(chips, mixingMethod = 'exact', w = c(1, 1, 1), names = TRUE)
 #' colorMixtureVenn(chips, mixingMethod = 'exact', w = c(10, 5, 1), names = TRUE)
 #' 
-#' 
-#' chips <- c('10YR 8/1', '2.5YR 3/6', '10YR 2/2', '5GY 6/6')
-#' names(chips) <- c("tan", "dark red", "dark brown", "light green")
-#' 
-#' colorMixtureVenn(chips, mixingMethod = 'exact', names = TRUE)
-#' colorMixtureVenn(chips, mixingMethod = 'exact', w = c(1, 1, 1, 2), names = TRUE)
-#' colorMixtureVenn(chips, mixingMethod = 'exact', w = c(10, 5, 2, 1), names = TRUE)
-#' colorMixtureVenn(chips, mixingMethod = 'exact', w = c(1, 2, 5, 10), names = TRUE)
-#'  
 #' }
 #' 
 

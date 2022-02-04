8ca7e34042894fb95de3ec6fe4195417865fb8d1
Dylan Beaudette <debeaudette@ucdavis.edu>
2022-02-04 00:59:16 -0800

    bah

--- misc/polar-WB-ideas.R
+++ misc/polar-WB-ideas.R
@@ -1,5 +1,5 @@
+library(soilDB)
 library(ggplot2)
-library(reshape2)
 
 s <- 'auburn'
 x <- fetchOSD(s, extended = TRUE)
@@ -13,44 +13,18 @@ PET <-  x$climate.monthly[x$climate.monthly$variable == 'Potential ET (mm)', ]
 PET$month <- structure(1:12, .Label = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"), class = "factor")
 
 
-
+# doesn't work... even though it seems like it should
 z <- rbind(
-  PPT[c(12, 1:12), ], 
-  PET[c(12, 1:12), ]
+  PPT[c(1:12, 1), ],
+  PET[c(1:12, 1), ]
   )
 
-z <- rbind(
-  PPT,
-  PET
-)
-
-# z.poly <- data.frame(
-#   variable = c(z$variable, z$variable),
-#   x = c(z$month, rev(z$month)),
-#   y = c(z$q75, rev(z$q25))
-# )
-
-i <- c(seq_along(z$variable), which(z$variable == "Precipitation (mm)"))
-
-z.poly <- rbind(
-  data.frame(z[i, c('month', 'variable')], CI = z$q25[i], type="q25") ,
-  data.frame(z[i, c('month', 'variable')], CI = z$q75[i], type="q75")
-)
-
-z.poly <- rbind(
-  data.frame(z[, c('month', 'variable')], CI = z$q25, type="q25") ,
-  data.frame(z[, c('month', 'variable')], CI = z$q75, type="q75")
-)
-
-z.poly
 
 
-ggplot(z, aes(x = month, y = q50, group = variable)) +  
-
-  geom_polygon(aes(group = variable, colour = variable), fill = NA, show.legend = FALSE) +  
-  geom_polygon(aes(x = month, y = CI, fill = variable, group = variable), data = z.poly, alpha = 0.4) +
-
-  coord_polar(theta="x", start = 0 , direction = 1) +
+ggplot(z, aes(x = month, y = q50, group = variable, color = variable)) +  
+  geom_line(show.legend = FALSE) + 
+  geom_ribbon(aes(ymin = q25, ymax = q75, fill = variable), alpha = 0.2) +
+  coord_polar(theta="x", start = pi/12 , direction = 1) +
   theme_light()
 
   

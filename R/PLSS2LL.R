dc4a47fa605c439ac39f4511c8c3648cc0fb1ba9
Andrew Brown <andrew.g.brown@usda.gov>
2022-03-23 18:46:34 -0700

    formatPLSS: Fix for section padding with some inputs

--- R/PLSS2LL.R
+++ R/PLSS2LL.R
@@ -98,7 +98,7 @@ formatPLSS <- function(p, type = 'SN') {
       p.r <- paste0(p.r, collapse = '0')
 
       # add 'SN to section number and pad single digit section numbers
-      p.s <- ifelse(is.na(p$s[i]), '', ifelse(p$s[i] > 9, paste0('SN', p$s[i]), paste0('SN', 0, p$s[i])))
+      p.s <- ifelse(is.na(p$s[i]), '', ifelse(p$s[i] > 9, p$s[i], paste0("0", p$s[i])))
 
       # replace NA -> '' IN S, Q and QQ sections
       p.q <- ifelse(is.na(p$q[i]), '', p$q[i])
@@ -107,7 +107,7 @@ formatPLSS <- function(p, type = 'SN') {
       # format the first chunk
       f.1 <- ifelse(nchar(p.s) == 0, 
                     paste0(paste0(c(p$m[i], p.t, p.r), collapse="0"), "0"), # no section
-                    paste0(c(p$m[i], p.t, p.r, p.s, 'A'), collapse = '0'))       # with section
+                    paste0(c(p$m[i], p.t, p.r, paste0("SN", "0", p.s, 'A')), collapse = '0'))       # with section
       
       # format the (optional) Q and QQ sections
       f.2 <- paste0(p.qq, p.q)

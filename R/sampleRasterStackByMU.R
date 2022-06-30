73f2603d5f45a2e1f0f8f88d646107d4472c1609
Andrew G. Brown <andrew.g.brown@usda.gov>
2022-06-30 15:37:57 -0700

    Use terra for sampleRasterStackByMU

--- R/sampleRasterStackByMU.R
+++ R/sampleRasterStackByMU.R
@@ -60,8 +60,8 @@ sampleRasterStackByMU <- function(mu,
     
   
   # sanity check: package requirements
-  if(!requireNamespace('rgdal') | !requireNamespace('rgeos') | !requireNamespace('raster') | !requireNamespace('spdep'))
-    stop('please install the packages: rgdal, rgeos, raster, spdep', call. = FALSE)
+  # if (!requireNamespace('rgdal') | !requireNamespace('rgeos') | !requireNamespace('raster') | !requireNamespace('spdep'))
+  #   stop('please install the packages: rgdal, rgeos, raster, spdep', call. = FALSE)
   
   # enforce projected CRS
   if (!is.projected(mu))
@@ -76,7 +76,11 @@ sampleRasterStackByMU <- function(mu,
   }
   
   # check for invalid geometry
-  validity.res <- data.frame(id=mu[[mu.col]], Polygon.Validity=rgeos::gIsValid(mu, byid=TRUE, reason=TRUE), stringsAsFactors = FALSE)
+  validity.res <- data.frame(
+      id = mu[[mu.col]],
+      Polygon.Validity = rgeos::gIsValid(mu, byid = TRUE, reason = TRUE),
+      stringsAsFactors = FALSE
+    )
   
   # init containers for intermediate results
   l.mu <- list() # samples
@@ -84,14 +88,12 @@ sampleRasterStackByMU <- function(mu,
   a.mu <- list() # area stats
   
   # load pointers to raster data
-  raster.list <- rapply(raster.list, how='replace', f=function(i) {
-    i <- try(raster::raster(i))
-    if(inherits(i, 'try-error'))
+  raster.list <- rapply(raster.list, how = 'replace', f = function(i) {
+    i <- try(terra::rast(i))
+    if (inherits(i, 'try-error')) {
       stop(paste0('Cannot find raster file: ', i), call. = FALSE)
-    else
-      return(i)
+    } else return(i)
   })
-  
   
   ##
   ## iterate over rasters and read into memory if possible
@@ -104,61 +106,42 @@ sampleRasterStackByMU <- function(mu,
   ## TODO: not sure how to get a progress bar for this...
   # https://cran.r-project.org/web/packages/pbapply/index.html
   
-  # recursively iterate over sets of raster data, attempting to load into memory
-  raster.list <- rapply(raster.list, how='replace', f=function(r) {
-    # attempt reading into memory
-    r.mem <- try(raster::readAll(r), silent = TRUE)
-    # if successful, return pointer to inMemory version
-    if(inherits(r.mem, 'RasterLayer')) {
-      return(r.mem)
-    } else {
-      # if not possible, retain the original file-based pointer
-      return(r)
-    }
-  })
-  
-  
-  ##
+  ##s
   ## check that raster extent completely covers mu extent
   ##
   
   # get MU extent, in original CRS
-  e.mu <- as(raster::extent(mu), 'SpatialPolygons')
-  proj4string(e.mu) <- proj4string(mu)
+  v.mu <- terra::vect(mu)
+  e.mu <- terra::as.polygons(terra::ext(v.mu), crs = terra::crs(v.mu))
   
   message('Checking raster/MU extents...')
   
   # recursively iterate over sets of raster data, checking extents
-  raster.containment.test <- rapply(raster.list, f=function(r) {
+  raster.containment.test <- rapply(raster.list, f = function(r) {
     # get current raster extent in original CRS
-    e.r <-as(raster::extent(r), 'SpatialPolygons')
-    proj4string(e.r) <- proj4string(r)
-    
+    e.r <- terra::as.polygons(terra::ext(r), crs = terra::crs(r))
+
     # transform MU extent to CRS of current raster
-    e.mu.r <- spTransform(e.mu, CRS(proj4string(e.r)))
-    
+    e.mu.r <- as(terra::project(e.mu, terra::crs(e.r)), 'Spatial')
+
     # check for complete containment of MU by current raster
-    return(rgeos::gContainsProperly(e.r, e.mu.r))
+    return(rgeos::gContainsProperly(as(e.r, 'Spatial'), e.mu.r))
   })
   
-  
-  ## TODO: finish this
-  if(any(! raster.containment.test))
+  if (any(!raster.containment.test))
     warning('Raster extent does not completly cover MU extent')
-  
   
   ## Moran's I by raster
   # result is a data.frame based on sampling of the raster sources, after cropping to mu extent
-  if(estimateEffectiveSampleSize) {
+  if (estimateEffectiveSampleSize) {
     message('Estimating effective sample size...')
     # note: this only makes sense for "continuous" type raster data sources
     MI <- ldply(raster.list$continuous, Moran_I_ByRaster, mu.extent = e.mu, .progress = ifelse(progress, 'text', NULL))
     names(MI) <- c('Variable', 'Moran.I')
   } else {
     # when Moran's I is 0, n_effective = n
-    MI <- data.frame(Variable=nm, Moran.I=0)
+    MI <- data.frame(Variable = nm, Moran.I = 0)
   }
-  
   
   ##
   ## iterate over map units, sample, extract raster values
@@ -166,16 +149,24 @@ sampleRasterStackByMU <- function(mu,
   message('Sampling polygons, and extracting raster values...')
   
   # progress bar
-  if(progress)
-    pb <- txtProgressBar(min=0, max=length(mu.set), style=3)
+  if (progress)
+    pb <- txtProgressBar(min = 0, max = length(mu.set), style = 3)
   
-  for(mu.i in mu.set) {
+  for (mu.i in mu.set) {
     # get current polygons
     mu.i.sp <- mu[which(mu[[mu.col]] == mu.i), ]
     
-    ## messages are issued when it isn't possibe to place the requested num. samples in a polygon
+    ## messages are issued when it isn't possible to place the requested num. samples in a polygon
     # sample each polygon at a constant density
-    suppressMessages(s <- constantDensitySampling(mu.i.sp, n.pts.per.ac=pts.per.acre, min.samples=1, polygon.id, iterations=iterations))
+    try(suppressMessages({
+      s <- constantDensitySampling(
+          mu.i.sp,
+          n.pts.per.ac = pts.per.acre,
+          min.samples = 1,
+          polygon.id,
+          iterations = iterations
+        )
+    }))
     
     # keep track of un-sampled polygons
     l.unsampled[[mu.i]] <- setdiff(mu.i.sp$pID, unique(s$pID))
@@ -183,12 +174,12 @@ sampleRasterStackByMU <- function(mu,
     ## ADDED 2016-08-27 to account for very small polygons
     ## see related fix in constantDensitySampling
     # if there are no samples, skip stats
-    if(is.null(s)) {
+    if (is.null(s)) {
       # save and continue
       a.mu[[mu.i]] <- NULL
       l.mu[[mu.i]] <- NULL
       
-      if(progress)
+      if (progress)
         setTxtProgressBar(pb, match(mu.i, mu.set))
       # break out of current iteration, move to next map unit
       next
@@ -197,23 +188,16 @@ sampleRasterStackByMU <- function(mu,
       # make a unique sample ID, need this for conversion: long -> wide
       s$sid <- 1:nrow(s)
       
-      ## TODO: test parallel processing here
-      # http://www.guru-gis.net/extract-raster-values-in-parallel/
-      
-      ## TODO: figure out Rast I/O errors sometimes generated here:
-      # https://github.com/ncss-tech/soilReports/issues/48
-      
       # extract raster data, sample ID, polygon ID to DF
-      l.mu[[mu.i]] <- rapply(raster.list, how = 'replace', f=function(r) {
-        res <- data.frame(value=raster::extract(r, s), pID=s$pID, sid=s$sid)
-        return(res)
+      l.mu[[mu.i]] <- rapply(raster.list, how = 'replace', f = function(r) {
+        cbind(value = terra::extract(r, terra::vect(s))[[2]], data.frame(pID = s$pID, sid = s$sid))
       })
       
       # extract polygon areas as acres
       a <- sapply(slot(mu.i.sp, 'polygons'), slot, 'area') * 2.47e-4
       
       # compute additional summaries
-      .quantiles <- quantile(a, probs=p)
+      .quantiles <- quantile(a, probs = p)
       .total.area <- sum(a)
       .samples <- nrow(s)
       .mean.sample.density <- round(.samples / .total.area, 2)
@@ -235,13 +219,13 @@ sampleRasterStackByMU <- function(mu,
       # save stats to list indexed by map unit ID
       a.mu[[mu.i]] <- a.stats
       
-      if(progress)
+      if (progress)
         setTxtProgressBar(pb, match(mu.i, mu.set))
     }
     
   } # done iterating over map units
   
-  if(progress)
+  if (progress)
     close(pb)
   
   # assemble MU area stats
@@ -271,7 +255,7 @@ sampleRasterStackByMU <- function(mu,
       # extract pieces
       d <- ldply(j)
       # check to make sure there are some rows to process
-      if(nrow(d) > 0) {
+      if (nrow(d) > 0) {
         # fix names
         names(d)[1] <- 'variable'
         # remove NA
@@ -298,16 +282,16 @@ sampleRasterStackByMU <- function(mu,
   unsampled.idx <- unlist(l.unsampled)
   
   # get raster summary
-  rs <- rapply(raster.list, f=raster::filename, how = 'unlist')
+  rs <- rapply(raster.list, f = terra::sources, how = 'unlist')
   rs <- gsub('\\\\', '/', rs)
   
   rs.df <- data.frame(Variable = nm, File = rs, 
-                      Resolution = as.character(rapply(raster.list, function(x) signif(raster::res(x)[1], 2), how = 'unlist')), 
-                      inMemory = as.character(rapply(raster.list, f = raster::inMemory, how = 'unlist')), 
+                      Resolution = as.character(rapply(raster.list, function(x) signif(terra::res(x)[1], 2), how = 'unlist')), 
+                      inMemory = as.character(rapply(raster.list, f = terra::inMemory, how = 'unlist')), 
                       ContainsMU = raster.containment.test)
   
   # join-in Moran's I
-  rs.df <- join(rs.df, MI, by='Variable', type='left')
+  rs.df <- join(rs.df, MI, by = 'Variable', type = 'left')
   
   # replace missing Moran's I with 0
   # this should only affect categorical / circular variables
@@ -315,7 +299,15 @@ sampleRasterStackByMU <- function(mu,
   
   
   # combine into single object and result
-  return(list('raster.samples'=d.mu, 'area.stats'=mu.area, 'unsampled.ids'=unsampled.idx, 'raster.summary'=rs.df, 'mu.validity.check'=validity.res, 'Moran_I'=MI))
+  return(list(
+      'raster.samples' = d.mu,
+      'area.stats' = mu.area,
+      'unsampled.ids' = unsampled.idx,
+      'raster.summary' = rs.df,
+      'mu.validity.check' = validity.res,
+      'Moran_I' = MI
+    )
+  )
 }
 
 

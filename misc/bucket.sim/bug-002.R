287fe449fa353110afe745a08b660ba24c17dd7e
Beaudette <dylan.beaudette@usda.gov>
2024-03-19 13:40:24 -0700

    updated dev code

--- misc/bucket.sim/bug-002.R
+++ misc/bucket.sim/bug-002.R
@@ -3,28 +3,48 @@
 
 # https://github.com/josephguillaume/hydromad/issues/190
 
-# source code: 
-# https://github.com/josephguillaume/hydromad/blob/master/R/bucket.R
-# 
-
-
 
 # test with Morley WB:
 # https://casoilresource.lawr.ucdavis.edu/sde/?series=morley#water-balance
 
+
+library(sharpshootR)
 library(hydromad)
 
 # initial conditions
+S_0 <- 1
 AWC <- 198
 PPT <- c(0, 0, 0, 0, 60, 100, 70, 0, 0, 0, 0, 0)
 PET <- c(0, 0, 0, 0, 120, 120, 60, 0, 0, 0, 0, 0)
+
+
+S_0 <- 1
+AWC <- 200
+PPT <- c(0, 0, 0, 0, 60, 100, 70, 0, 0, 0, 0, 0)
+PET <- c(0, 0, 0, 0, 120, 120, 120, 0, 0, 0, 0, 0)
+
+
+# 
+S_0 <- 1
+AWC <- 100
+PPT <- c(20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+PET <- c(0, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20)
+
+# #
+# S_0 <- 1
+# AWC <- 100
+# PPT <- c(0, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0)
+# PET <- c(0, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20)
+
+
+hydromad.options('pure.R.code' = TRUE)
 
 d <- data.frame(P = PPT, E = PET)
 
 m <- hydromad::hydromad(d[, 1:2], sma = "bucket", routing = NULL)
 
 # start with "full" bucket
-m <- update(m, Sb = AWC, fc = 1, S_0 = 1, a.ss = 0.01, M = 0, etmult = 1, a.ei = 0)
+m <- update(m, Sb = AWC, fc = 0.5, S_0 = S_0, a.ss = 0, M = 0.5, etmult = 1, a.ei = 0)
 res <- predict(m, return_state = TRUE)
 
 # combine climate inputs + water balance
@@ -34,4 +54,18 @@ res <- cbind(d, res)
 res$D <- res$ET - res$E
 
 knitr::kable(res)
+
+## TODO: check mass balance
+sum(c(0, diff(res$S))) + sum(res$P) - sum(res$ET)
+
+
+w <- monthlyWB(AWC = AWC, PPT = PPT, PET = PET, S_init = S_0, rep = 1)
+# monthlyWB_summary(w)
+plotWB(w)
+
+
+
+
+
+
 
---
+++ misc/bucket.sim/distribute-monthly-values-test.R
@@ -0,0 +1,124 @@
+library(hydromad)
+
+
+## near Sonora, CA
+S_0 <- 1
+AWC <- 100
+PET <- c(0, 0, 5,80, 90, 120, 130, 140, 110, 90, 20, 5)
+PPT <- c(0, 150, 200, 120, 20, 0, 0, 0, 10, 20, 30, 60)
+
+
+## huge pulse of rain, constant PET
+S_0 <- 1
+AWC <- 100
+PPT <- c(100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+PET <- c(0, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20)
+
+## 
+S_0 <- 1
+AWC <- 198
+PPT <- c(0, 0, 0, 0, 60, 100, 70, 0, 0, 0, 0, 0)
+PET <- c(0, 0, 0, 0, 120, 120, 60, 0, 0, 0, 0, 0)
+
+
+## huge pulse of rain, and deficit beyond AWC? 
+## results seem wrong
+S_0 <- 1
+AWC <- 100
+PPT <- c(20, 0, 0, 0, 0, 0, 0, 100, 0, 0, 0, 0)
+PET <- c(0, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20)
+
+
+## Coweeta
+S_0 <- 1
+AWC <- 35
+PPT <- c(200, 200, 183, 159, 158, 164, 140, 139, 173, 140, 204, 194)
+PET <- c(4, 9, 27, 52, 83, 111, 125, 116, 83, 50, 23, 8)
+
+# S_0 <- 1
+# AWC <- 100
+# PPT <- c(200, 200, 183, 159, 158, 164, 140, 139, 173, 140, 204, 194)
+# PET <- c(4, 9, 27, 52, 83, 111, 125, 116, 83, 50, 23, 8)
+
+
+
+
+
+
+w1 <- monthlyWB(AWC = AWC, PPT = PPT, PET = PET, S_init = S_0, rep = 3, keep_last = TRUE, distribute = FALSE)
+
+w2 <- monthlyWB(AWC = AWC, PPT = PPT, PET = PET, S_init = S_0, rep = 3, keep_last = TRUE, distribute = TRUE, k = 10, method = 'equal')
+
+w3 <- monthlyWB(AWC = AWC, PPT = PPT, PET = PET, S_init = S_0, rep = 3, keep_last = TRUE, distribute = TRUE, k = 10, method = 'random')
+
+
+par(mfrow = c(1, 2), mar = c(4, 3.5, 3, 2))
+plotWB(w1, legend.cex = 0.7, month.cex = 0.8) ; title('Monthly Totals', cex.main = 1)
+plotWB(w2, legend.cex = 0.7, month.cex = 0.8) ; title('Distributed', cex.main = 1)
+
+
+
+par(mfrow = c(1, 3))
+
+plotWB(w1)
+plotWB(w2)
+plotWB(w3)
+
+plotWB_lines(w1)
+plotWB_lines(w2)
+plotWB_lines(w3)
+
+
+
+
+## water year check
+
+s <- 'amador'
+x <- fetchOSD(s, extended = TRUE)
+
+# get representative, profile-total AWC from SSURGO
+sql <- sprintf("
+SELECT chorizon.cokey AS cokey, 
+SUM(awc_r * (hzdepb_r - hzdept_r)) AS ws 
+FROM 
+legend JOIN mapunit ON legend.lkey = mapunit.lkey
+JOIN component ON mapunit.mukey = component.mukey
+JOIN chorizon ON component.cokey = chorizon.cokey 
+WHERE compname = '%s'
+AND areasymbol != 'US'
+GROUP BY chorizon.cokey;", s
+)
+
+# get via SDA
+res <- SDA_query(sql)
+
+# median AWC in mm
+# over all components correlated to named series 
+AWC <- round(median(res$ws, na.rm = TRUE) * 10)
+
+
+# monthly climate data from series summary
+PPT <- x$climate.monthly$q50[x$climate.monthly$variable == 'Precipitation (mm)']
+PET <- x$climate.monthly$q50[x$climate.monthly$variable == 'Potential ET (mm)']
+
+
+w1 <- monthlyWB(AWC, PPT, PET, S_init = 0, starting_month = 9, rep = 3, keep_last = TRUE)
+w2 <- monthlyWB(AWC, PPT, PET, S_init = 0, starting_month = 9, rep = 3, keep_last = TRUE, distribute = TRUE)
+
+par(mfrow = c(1, 2), mar = c(4, 3.5, 3, 2))
+plotWB(w1, legend.cex = 0.7, month.cex = 0.8) ; title('Monthly Totals', cex.main = 1)
+plotWB(w2, legend.cex = 0.7, month.cex = 0.8) ; title('Distributed', cex.main = 1)
+
+
+
+w1 <- monthlyWB(AWC, PPT, PET, S_init = 0, starting_month = 1, rep = 3, keep_last = TRUE)
+w2 <- monthlyWB(AWC, PPT, PET, S_init = 0, starting_month = 1, rep = 3, keep_last = TRUE, distribute = TRUE)
+
+par(mfrow = c(1, 2), mar = c(4, 3.5, 3, 2))
+plotWB(w1, legend.cex = 0.7, month.cex = 0.8) ; title('Monthly Totals', cex.main = 1)
+plotWB(w2, legend.cex = 0.7, month.cex = 0.8) ; title('Distributed', cex.main = 1)
+
+
+
+
+

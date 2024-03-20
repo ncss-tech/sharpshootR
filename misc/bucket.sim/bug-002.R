44f4e2e3aad7aeb6750c0fc54a209fd7bedfe05c
Beaudette <dylan.beaudette@usda.gov>
2024-03-20 11:26:57 -0700

    further tests related to #54 which may not be a bug after all

--- misc/bucket.sim/bug-002.R
+++ misc/bucket.sim/bug-002.R
@@ -10,6 +10,49 @@
 
 library(sharpshootR)
 library(hydromad)
+library(aqp)
+library(soilDB)
+
+
+## original test case, MORLEY series
+
+s <- 'morley'
+x <- fetchOSD(s, extended = TRUE)
+
+# get representative, profile-total AWC from SSURGO
+sql <- sprintf("
+SELECT chorizon.cokey AS cokey, 
+SUM(awc_r * (hzdepb_r - hzdept_r)) AS ws 
+FROM 
+legend JOIN mapunit ON legend.lkey = mapunit.lkey
+JOIN component ON mapunit.mukey = component.mukey
+JOIN chorizon ON component.cokey = chorizon.cokey 
+WHERE compname = '%s'
+AND areasymbol != 'US'
+GROUP BY chorizon.cokey;", s
+)
+
+# get via SDA
+res <- SDA_query(sql)
+
+# median AWC in mm
+# over all components correlated to named series 
+AWC <- round(median(res$ws, na.rm = TRUE) * 10)
+
+# monthly climate data from series summary
+PPT <- x$climate.monthly$q50[x$climate.monthly$variable == 'Precipitation (mm)']
+PET <- x$climate.monthly$q50[x$climate.monthly$variable == 'Potential ET (mm)']
+
+S_0 <- 1
+
+
+w <- monthlyWB(AWC = AWC, PPT = PPT, PET = PET, S_init = S_0, rep = 1, keep_last = TRUE, distribute = FALSE)
+plotWB(w)
+
+attr(w, 'mass.balance')
+
+
+
 
 # initial conditions
 S_0 <- 1
@@ -37,6 +80,9 @@ PET <- c(0, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20)
 # PET <- c(0, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20)
 
 
+
+
+
 hydromad.options('pure.R.code' = TRUE)
 
 d <- data.frame(P = PPT, E = PET)
@@ -44,7 +90,7 @@ d <- data.frame(P = PPT, E = PET)
 m <- hydromad::hydromad(d[, 1:2], sma = "bucket", routing = NULL)
 
 # start with "full" bucket
-m <- update(m, Sb = AWC, fc = 0.5, S_0 = S_0, a.ss = 0, M = 0.5, etmult = 1, a.ei = 0)
+m <- update(m, Sb = AWC, fc = 1, S_0 = S_0, a.ss = 0.01, M = 0, etmult = 1, a.ei = 0)
 res <- predict(m, return_state = TRUE)
 
 # combine climate inputs + water balance
@@ -55,13 +101,35 @@ res$D <- res$ET - res$E
 
 knitr::kable(res)
 
-## TODO: check mass balance
-sum(c(0, diff(res$S))) + sum(res$P) - sum(res$ET)
+
+## test mass balance
+
+# P = U + ET + dS
+# for all time steps: .in = .out + dS
+# final mass balance: sum(.in - (.out + .dS))
+
+# PPT
+.in <- res$P
+
+# AET + U
+.out <- res$ET + res$U
+
+# dS
+# more complicated than seems, when S_init < 1
+# S at time 0 = AWC * S_init 
+.dS <- diff(c(AWC * S_0, res$S))
+
+# mass balance
+(.mb <- sum(.in - (.out + .dS)))
 
 
-w <- monthlyWB(AWC = AWC, PPT = PPT, PET = PET, S_init = S_0, rep = 1)
-# monthlyWB_summary(w)
+
+
+w <- monthlyWB(AWC = AWC, PPT = PPT, PET = PET, S_init = S_0, rep = 1, keep_last = TRUE, distribute = TRUE)
 plotWB(w)
+
+attr(w, 'mass.balance')
+attr(w, 'AWC')
 
 
 
